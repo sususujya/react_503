b928873dc489fd50954fe6bad278602bddf7d124
Master Kang <sususujya@gmail.com>
2022-07-04 21:48:38 +0900

    220704

--- react_01/src/Header.js
+++ react_01/src/Header.js
@@ -1,11 +1,17 @@
 function Header() {
 
   const leevar = <h2>Hello World! TGIM</h2>
+  let TR = true;
+  // let TR = null;
+
   return (
     <>
       <i className="xi-heart"></i>
       {leevar}
+      {TR ? 'cham' : 'gula'}
       <div>React beginner</div>
+      <div className={TR ? 'on' : 'off'}>WA!!!!</div>
+      <div className={TR && 'on'}>WA!!!! rururuururu</div>
     </>
   );
 }
