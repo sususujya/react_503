9179f7ad49975fb6d8dbd1cf2b1df41e951b91a9
Master Kang <sususujya@gmail.com>
2022-07-04 21:18:59 +0900

    220704

---
+++ react_01/src/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+
+  const leevar = <h2>Hello World!</h2>
+  return (
+    <>
+      {leevar}
+      <div>My first react Ha..HA....ha......HAH!!!</div>
+    </>
+  );
+}
+
+export default Header;
