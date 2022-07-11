598c16e05d9c17074d6153913df104af7a05e512
Master Kang <alohasujya@gmail.com>
2022-07-11 21:42:45 +0900

    220711

--- study_02/src/Map.js
+++ study_02/src/Map.js
@@ -1,13 +1,23 @@
 const Map = () => {
     const DATA = [
-        { id: 1, }
+        { id: 1, name: "LT", title: "Best Songwriter Ever", content: "Everybody waiting for his next album", src: './img/lesedilarona01.jpg' },
+        { id: 2, name: "Louis", title: "Best Singer", content: "Everybody admire his next album", src: './img/lesedilarona02.jpg' },
+        { id: 3, name: "Tomlinson", title: "Best Artist", content: "Everybody joined his album", src: './img/lesedilarona03.jpg' },
+        { id: 4, name: "LouisTomlinson", title: "Best Perfomer", content: "Everybody love his album", src: './img/lesedilarona04.jpg' },
     ]
     return (
         <>
             <ul>
-                <li>01</li>
-                <li>02</li>
-                <li>03</li>
+                {
+                    // DATA.map(elm => <li>{elm.name} is {elm.title} and {elm.content}</li>)
+                    DATA.map((elm, idx) =>
+                        <li key={elm.id}>
+                            <img src={elm.src} alt="WOW" />
+                            {elm.name} is {elm.title} and '{elm.content}'
+                        </li>
+                    )
+                    // ^^^ 보통은 안 쓴다
+                }
             </ul>
         </>
     )
